e78a6432e6ddd3f99de1c2c8081d499246f00e50
Tanya <taniyhamail@gmail.ua>
2023-08-14 13:09:12 +0300

    feat: added header

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <main className="max-w-screen-2xl m-auto bg-white">
         {/* NAVBAR */}
         <nav className="bg-white p-2 flex justify-between">
-          <a href="" className="font-bold text-gray-700 text-2xl">
+          <a href="#" className="font-bold text-gray-700 text-2xl">
             OpenTable
           </a>
           <div>
@@ -18,11 +18,22 @@ export default function Home() {
               <button className="bg-blue-400 text-white p-1 px-4 rounded mr-3">
                 Sing in
               </button>
-              <button className="  p-1 px-4 rounded">Sing up</button>
+              <button className="p-1 px-4 rounded">Sing up</button>
             </div>
           </div>
         </nav>
         {/* NAVBAR */}
+        <main>
+          {/* HEADER */}
+          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+            <div className="text-center mt-10">
+              <h1 className="text-white text-5xl font-bold mb-2">
+                Find you table for any occasion
+              </h1>
+            </div>
+          </div>
+          {/* HEADER */}
+        </main>
       </main>
     </main>
   )
