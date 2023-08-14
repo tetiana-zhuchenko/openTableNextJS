dedb13509ac6215081dd29686ea13efd1561e215
Tanya <taniyhamail@gmail.ua>
2023-08-14 12:51:11 +0300

    feat: added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 import Image from 'next/image'
-import { Inter } from "next/font/google"
+import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className="text-blue-400 text-7xl">Hello there from Next 13</h1>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/* NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="" className="font-bold text-gray-700 text-2xl">
+            OpenTable
+          </a>
+          <div>
+            <div className="flex">
+              <button className="bg-blue-400 text-white p-1 px-4 rounded mr-3">
+                Sing in
+              </button>
+              <button className="  p-1 px-4 rounded">Sing up</button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+      </main>
     </main>
   )
 }
