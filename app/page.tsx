7e132d3b531fc78677b4ad9282ce18c5684f922e
Tanya <taniyhamail@gmail.ua>
2023-08-15 13:07:46 +0300

    feat: added Search Bar and Reastaurant Card

--- app/page.tsx
+++ app/page.tsx
@@ -5,36 +5,5 @@ import styles from './page.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        {/* NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-          <a href="#" className="font-bold text-gray-700 text-2xl">
-            OpenTable
-          </a>
-          <div>
-            <div className="flex">
-              <button className="bg-blue-400 text-white p-1 px-4 rounded mr-3">
-                Sing in
-              </button>
-              <button className="p-1 px-4 rounded">Sing up</button>
-            </div>
-          </div>
-        </nav>
-        {/* NAVBAR */}
-        <main>
-          {/* HEADER */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-5xl font-bold mb-2">
-                Find you table for any occasion
-              </h1>
-            </div>
-          </div>
-          {/* HEADER */}
-        </main>
-      </main>
-    </main>
-  )
+  return <main></main>
 }
