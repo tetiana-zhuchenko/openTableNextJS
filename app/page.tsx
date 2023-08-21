87c6c7872be5ffabd21b14fc7af4f93cd473d328
Tanya <taniyhamail@gmail.ua>
2023-08-21 13:04:22 +0300

    feat: added programmatic navigation with the useRouter Hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter()
+  const [location, setLocation] = useState('')
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
@@ -37,8 +43,16 @@ export default function Home() {
                   type="text"
                   className="rounded mr-3 p-2 w-[350px]"
                   placeholder="State, city or town"
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
                 />
-                <button className="bg-red-600 rounded px-9 py-2 text-white">
+                <button
+                  className="bg-red-600 rounded px-9 py-2 text-white"
+                  onClick={() => {
+                    if (location === 'banana') return
+                    router.push('/search')
+                  }}
+                >
                   Let's go
                 </button>
               </div>
