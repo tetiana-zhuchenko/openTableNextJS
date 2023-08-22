b7c91e8b790f1e303fd8d9355eeab5c1a1c3a54e
Tanya <taniyhamail@gmail.ua>
2023-08-22 16:24:20 +0300

    feat: added nested layout

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,16 +1,12 @@
-import Header from '../components/Header'
 import Menu from '../components/Menu'
 import RestaurantNavBar from '../components/RestaurantNavBar'
 
 export default function RestaurantMenu() {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[100%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Menu />
-        </div>
+      <div className="bg-white w-[100%] rounded p-3 shadow">
+        <RestaurantNavBar />
+        <Menu />
       </div>
     </>
   )
