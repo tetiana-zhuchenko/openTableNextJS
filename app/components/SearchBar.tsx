c240c2f4c4c809071a1ec8cf83fcefa11bf73939
Tanya <taniyhamail@gmail.ua>
2023-08-22 12:45:30 +0300

    feat: devided components into server and client

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+export default function SearchBar() {
+  const router = useRouter()
+  const [location, setLocation] = useState('')
+  return (
+    <div className="text-left text-lg py-3 m-auto flex justify-center">
+      <input
+        type="text"
+        className="rounded mr-3 p-2 w-[350px]"
+        placeholder="State, city or town"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button
+        className="bg-red-600 rounded px-9 py-2 text-white"
+        onClick={() => {
+          if (location === 'banana') return
+          router.push('/search')
+        }}
+      >
+        Let's go
+      </button>
+    </div>
+  )
+}
--- app/search/components/Header.tsx
+++ app/search/components/Header.tsx
@@ -1,18 +1,9 @@
+import SearchBar from '../../components/SearchBar'
+
 export default function Header() {
   return (
     <div className="bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-      {/* SEARCH BAR */}
-      <div className="text-left text-lg py-3 m-auto flex justify-center">
-        <input
-          type="text"
-          className="rounded mr-3 p-2 w-[350px]"
-          placeholder="State, city or town"
-        />
-        <button className="bg-red-600 rounded px-9 py-2 text-white">
-          Let's go
-        </button>
-      </div>
-      {/* SEARCH BAR */}
+      <SearchBar />
     </div>
   )
 }
