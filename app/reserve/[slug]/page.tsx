cdb2ec207da04ddbefcf8124c1c2f56e68dd5a28
Tanya <taniyhamail@gmail.ua>
2023-08-22 13:04:59 +0300

    feat: improved layout file

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,19 +1,15 @@
-import NavBar from '../../components/NavBar'
 import Header from './components/Header'
 import Form from './components/Form'
 
 export default function Reserve() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <div className="border-t h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Header />
-            <Form />
-          </div>
+    <>
+      <div className="border-t h-screen">
+        <div className="py-9 w-3/5 m-auto">
+          <Header />
+          <Form />
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   )
 }
