c9431a17c11cbcae8b7340c52ae4a8c3da3514d0
Tanya <taniyhamail@gmail.ua>
2023-08-25 14:44:12 +0300

    feat: added fetching a restaurant by slug

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -5,8 +5,42 @@ import Description from './components/Description'
 import Images from './components/Images'
 import Reviews from './components/Reviews'
 import ReservationCard from './components/ReservationCard'
+import { PrismaClient } from '@prisma/client'
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient()
+
+interface Restaurant {
+  id: number
+  name: string
+  images: string[]
+  description: string
+  slug: string
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  })
+  if (!restaurant) throw new Error()
+  return restaurant
+}
+
+export default async function RestaurantDetails({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug)
+  console.log(restaurant)
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
