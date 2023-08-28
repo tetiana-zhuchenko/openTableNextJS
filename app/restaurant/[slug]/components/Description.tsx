3add6e64ee6d6837233dc48ca9bea132db853bc8
Tanya <taniyhamail@gmail.ua>
2023-08-28 12:54:04 +0300

    feat: populated the data in the restaurant page

--- app/restaurant/[slug]/components/Description.tsx
+++ app/restaurant/[slug]/components/Description.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 
-export default function Description() {
+export default function Description({ description }: { description: string }) {
   return (
     <div className="mt-4">
-      <p className="text-lg font-light">
-        Indulge in Northern Italian Cuisine with an air of European
-        sophistication and influence at Subito. Savor homemade pastas,
-        scratch-made sauces and hand-selected, perfectly seared steaks, finished
-        in our infrared broiler. Along with the freshest seafood, flown in daily
-        from the ports, Subito offers brick oven pizzas, a raw bar and
-        traditional antipasti.
-      </p>
+      <p className="text-lg font-light">{description}</p>
     </div>
   )
 }
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -40,15 +40,15 @@ export default async function RestaurantDetails({
   params: { slug: string }
 }) {
   const restaurant = await fetchRestaurantBySlug(params.slug)
-  console.log(restaurant)
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name} />
         <Rating />
-        <Description />
-        <Images />
+        <Description description={restaurant.description} />
+        <Images images={restaurant.images} />
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
