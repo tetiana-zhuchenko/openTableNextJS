f2be9225889ce0ae04c47d10e0358e253cd22122
Tanya <taniyhamail@gmail.ua>
2023-09-18 20:41:59 +0300

    feat: added loading state

---
+++ app/loading.tsx
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+
+export default function Loading() {
+  return (
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((number) => (
+          <div
+            key={number}
+            className="animate-pulse bg-slate-200 w-64 h-72 m-3 rounded overflow-hiden border cursor-pointer"
+          ></div>
+        ))}
+      </div>
+    </main>
+  )
+}
+;<div className="animate-pulse"></div>
