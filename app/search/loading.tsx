3ed9e8c121d0258b6a6854dbc6cd00b5ce2599e2
Tanya <taniyhamail@gmail.ua>
2023-09-18 21:18:11 +0300

    feat: added overriding the default loading state

---
+++ app/search/loading.tsx
@@ -0,0 +1,11 @@
+import Header from './components/Header'
+
+export default function Loading() {
+  return (
+    <main>
+      <div className="overflow-hidden animate-pulse bg-slate-200">
+        <Header />
+      </div>
+    </main>
+  )
+}
