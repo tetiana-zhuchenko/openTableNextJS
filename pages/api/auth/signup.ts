2461884cde7f0722cf10e97d1dc606f2caba58bb
Tanya <taniyhamail@gmail.ua>
2023-09-20 18:10:59 +0300

    feat: added validation that the user's doesen't already have an account

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import validator from 'validator'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default async function handler(
   req: NextApiRequest,
@@ -53,6 +56,18 @@ export default async function handler(
 
     if (errors.length) {
       return res.status(400).json({ errorMessage: errors[0] })
+    }
+
+    const userWithEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithEmail) {
+      return res
+        .status(400)
+        .json({ errorMessage: 'Email is associated with another account' })
     }
 
     res.status(200).json({
