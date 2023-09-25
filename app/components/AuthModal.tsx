c800c25394b89b65ccb4bc574677c4e057de18ac
Tanya <taniyhamail@gmail.ua>
2023-09-25 15:16:27 +0300

    feat: added client side validation

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
 import AuthModalInputs from './AuthModalInputs'
 
@@ -43,6 +41,29 @@ export default function AuthModal({ isSingin }: { isSingin: boolean }) {
     password: '',
   })
 
+  const [disabled, setDisabled] = useState(true)
+
+  useEffect(() => {
+    if (isSingin) {
+      if (inputs.password && inputs.email) {
+        return setDisabled(false)
+      }
+    } else {
+      if (
+        inputs.firstName &&
+        inputs.email &&
+        inputs.firstName &&
+        inputs.lastName &&
+        inputs.password &&
+        inputs.phone
+      ) {
+        return setDisabled(false)
+      }
+    }
+
+    setDisabled(true)
+  }, [inputs])
+
   return (
     <div>
       <button
@@ -79,7 +100,10 @@ export default function AuthModal({ isSingin }: { isSingin: boolean }) {
                 handleChangeInput={handleChangeInput}
                 isSingin={isSingin}
               />
-              <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400">
+              <button
+                className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400"
+                disabled={disabled}
+              >
                 {renderContent('Sing In', 'Create Account')}
               </button>
             </div>
