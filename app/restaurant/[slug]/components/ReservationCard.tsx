edcf8959f5c7df972e9e6c26bae08dfeb822bca7
Tanya <taniyhamail@gmail.ua>
2023-09-28 15:29:44 +0300

    feat: added rendering the party size options

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { partySize } from '../../../../data'
+
 export default function ReservationCard() {
   return (
     <div className="fixed w-[15%] bg-white rounded p-3 shadow">
@@ -9,8 +11,11 @@ export default function ReservationCard() {
       <div className="my-3 flex flex-col">
         <label htmlFor="">Party size</label>
         <select name="" id="" className="py-3 border-b font-light">
-          <option value="">1 person</option>
-          <option value="">2 people</option>
+          {partySize.map((size) => (
+            <option key={size.value} value={size.value}>
+              {size.label}
+            </option>
+          ))}
         </select>
       </div>
       <div className="flex justify-between">
