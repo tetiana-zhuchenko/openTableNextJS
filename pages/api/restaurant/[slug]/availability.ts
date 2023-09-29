118fb696f18502fa21123a16c398f6a2673cb051
Tanya <taniyhamail@gmail.ua>
2023-09-29 13:36:23 +0300

    feat: added determining the search time

---
+++ pages/api/restaurant/[slug]/availability.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { times } from '../../../../data'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { slug, day, time, partySize } = req.query as {
+    slug: string
+    day: string
+    time: string
+    partySize: string
+  }
+
+  if (!day || !time || !partySize) {
+    return res.status(400).json({ errorMessage: 'Invalid data provided' })
+  }
+
+  const searchTimes = times.find((t) => {
+    return t.time === time
+  })?.searchTimes
+
+  if (!searchTimes) {
+    return res.status(400).json({ errorMessage: 'Invalid data provided' })
+  }
+
+  return res.json({ searchTimes })
+}
+
+// http://localhost:3000/api/restaurant/ramakrishna-indian-restaurant-ottawa/availability?day=2023-01-01&time=20:00:00.000Z&partySize=4
