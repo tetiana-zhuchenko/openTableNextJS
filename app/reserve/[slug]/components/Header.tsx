19af8c7d75e2705a01257960ca63a8cb5825f5ae
Tanya <taniyhamail@gmail.ua>
2023-10-11 14:27:13 +0300

    feat: added dynamically rendering data for the Reserve Header

--- app/reserve/[slug]/components/Header.tsx
+++ app/reserve/[slug]/components/Header.tsx
@@ -1,10 +1,22 @@
+import {
+  Time,
+  convertToDisplayTime,
+} from '../../../../utils/convertToDisplayTime'
+import { format } from 'date-fns'
+
 export default function Header({
   image,
   name,
+  date,
+  partySize,
 }: {
   image: string
   name: string
+  date: string
+  partySize: string
 }) {
+  const [day, time] = date.split('T')
+
   return (
     <div>
       <h3 className="font-bold">You're almost done!</h3>
@@ -13,9 +25,11 @@ export default function Header({
         <div className="ml-4">
           <h1 className="text-3xl font-bold">{name}</h1>
           <div className="flex mt-3">
-            <p className="mr-6">Tuesday, 22, 2023</p>
-            <p className="mr-6">7:30 PM</p>
-            <p className="mr-6">3 people</p>
+            <p className="mr-6">{format(new Date(date), 'ccc, LLL d')}</p>
+            <p className="mr-6">{convertToDisplayTime(time as Time)}</p>
+            <p className="mr-6">
+              {partySize} {parseInt(partySize) === 1 ? 'person' : 'people'}
+            </p>
           </div>
         </div>
       </div>
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -21,15 +21,22 @@ const fetchRestaurantBySlug = async (slug: string) => {
 
 export default async function Reserve({
   params,
+  searchParams,
 }: {
   params: { slug: string }
+  searchParams: { date: string; partySize: string }
 }) {
   const restaurant = await fetchRestaurantBySlug(params.slug)
   return (
     <>
       <div className="border-t h-screen">
         <div className="py-9 w-3/5 m-auto">
-          <Header image={restaurant.main_image} name={restaurant.name} />
+          <Header
+            image={restaurant.main_image}
+            name={restaurant.name}
+            date={searchParams.date}
+            partySize={searchParams.partySize}
+          />
           <Form />
         </div>
       </div>
