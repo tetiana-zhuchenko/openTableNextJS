1873c587d1a7a73b5c153bca26c292217cfd2b1d
Tanya <taniyhamail@gmail.ua>
2023-10-11 14:04:56 +0300

    feat: added dynamically rendering data for the Reserve Header

--- app/reserve/[slug]/components/Header.tsx
+++ app/reserve/[slug]/components/Header.tsx
@@ -1,15 +1,17 @@
-export default function Header() {
+export default function Header({
+  image,
+  name,
+}: {
+  image: string
+  name: string
+}) {
   return (
     <div>
       <h3 className="font-bold">You're almost done!</h3>
       <div className="mt-5 flex">
-        <img
-          src="https://resizer.otstatic.com/v2/photos/xlarge/1/51434302.webp"
-          alt=""
-          className="w-32 h-18 rounded"
-        />
+        <img src={image} alt="" className="w-32 h-18 rounded" />
         <div className="ml-4">
-          <h1 className="text-3xl font-bold">Piccola Cucina</h1>
+          <h1 className="text-3xl font-bold">{name}</h1>
           <div className="flex mt-3">
             <p className="mr-6">Tuesday, 22, 2023</p>
             <p className="mr-6">7:30 PM</p>
